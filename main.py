21319160fc02b140021bec36b7bb49f8a0486682
alunogmfs2 <134213951+alunogmfs2@users.noreply.github.com>
2024-08-27 11:49:34 +0200

    Criei o Menu Modos

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from module import *
 from menus.principal import menuPrincipal
 from menus.calculadora import menuCalculadora
 from funcoes.calculadora.calculos import *
+from funcoes.modos.modos import *
+from menus.modos import *
 
 escolhaMain: int = -1
+escolhaModos: int = menuModos()
+
+if escolhaModos == 1:
+    abrirTudo(1)
+elif escolhaModos == 2:
+    abrirTudo(2)
+elif escolhaModos == 3:
+    abrirTudo(3)
+elif escolhaModos == 4:
+    abrirTudo(4)
+elif escolhaModos == 5:
+    abrirTudo(5)
+else:
+    exit()
 
 while True:
     if escolhaMain == 1:
